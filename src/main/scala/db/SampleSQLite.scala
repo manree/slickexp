507bc59fa7536c1ee6847966c57c0ee5a34036d8
Manuel Reenders <manuel.reenders@gmail.com>
2013-10-03 21:02:01 +0200

    First running version, create sqlite db, create table, populate, query

---
+++ src/main/scala/db/SampleSQLite.scala
@@ -0,0 +1,51 @@
+/**
+ *
+ */
+package db
+
+// Use SQLiteDriver to connect or create a sqlite database
+import scala.slick.driver.SQLiteDriver.simple._
+
+// Use the implicit threadLocalSession
+import Database.threadLocalSession
+
+/**
+ * @author manuel
+ * 
+ * Shows how to create a sqlite db, create a table, 
+ * populate table and run a query.
+ */
+object SampleSQLite {
+
+  def main(args: Array[String]): Unit = {
+
+    println("Here we go!")
+
+    Database.forURL("jdbc:sqlite:" + System.getProperty("user.dir") + "/testdb.db",
+      driver = "org.sqlite.JDBC") withSession {
+
+      // A simple table named Cats
+      object Cats extends Table[(Int, String)]("PERSONS") {
+        def id = column[Int]("ID", O.PrimaryKey) // The primary key column
+        def name = column[String]("NAME")
+         // Every table needs a * projection with the same type as the table's type parameter
+        def * = id ~ name
+      }
+      
+      // create the table
+      Cats.ddl.create
+      
+      // Insert some cats
+      Cats.insert(67, "Vlad")
+      Cats.insert(85, "Igor")
+      
+       // Iterate through all persons
+      Query(Cats) foreach {
+        case (id, name) =>
+          println("  " + name + "\t" + id)
+      }
+      
+      }
+  }
+
+}
